ebb3ecc71a6c2ff794523d8a4a43116d340f950b
Rustin Crandall <rustinpc@gmail.com>
2015-01-15 20:13:27 -0800

    two tests on one function passing.

--- public/js/utils/__tests__/chartHelpers-test.js
+++ public/js/utils/__tests__/chartHelpers-test.js
@@ -1,21 +1,23 @@
-// jest.dontMock('../chartHelpers');
-// jest.dontMock('react/lib/merge');
-
-var helper = require('../chartHelpers');
+jest.dontMock('../chartHelpers');
+jest.dontMock('react/lib/merge');
 
 describe('chartHelpers', function() {
+  var helper = require('../chartHelpers');
   describe('formatDonutChartData', function() {
+    var filteredData = [{primaryLabel: "Electronics", price: 999 / 100},{primaryLabel: "Books", price: 899 / 100},{primaryLabel: "Household", price: 10099 / 100},{primaryLabel: "Payments", price: 399 / 100},{primaryLabel: "Travel", price: 1000087 / 100},{primaryLabel: "Electronics", price: 10002 / 100},{primaryLabel: "Other", price: 1499 / 100},{primaryLabel: "Movies", price: 799 / 100},{primaryLabel: "Restaurants", price: 199 / 100},{primaryLabel: "Rent", price: 1299 / 100}];
     it('returns an array of 2 arrays', function() {
-      var filteredData = {
-        primaryLabel: "Electronics",
-        secondaryLabel: "Amazon",
-        price: 999 / 100,
-        date: "2014-12-20"
-      };
       var response = helper.formatDonutChartData(filteredData);
-      expect(typeof response).toEqual('Array');
-      expect(typeof response[0]).toEqual('Array');
-      expect(typeof response[1]).toEqual('Array');
+      expect(Array.isArray(response)).toEqual(true);
+      expect(Array.isArray(response[0])).toEqual(true);
+      expect(Array.isArray(response[1])).toEqual(true);
+      expect(response.length).toEqual(2);
+    });
+    it('both arrays are arrays of arrays', function() {
+      var response = helper.formatDonutChartData(filteredData);
+      expect(Array.isArray(response[0][1])).toEqual(true);
+      expect(response[0][0][0]).toEqual('Travel');
+      expect(response[0][1][1]).toEqual('110.01');
+      expect(Array.isArray(response[0][0])).toEqual(true);
       expect(response.length).toEqual(2);
     });
   });
